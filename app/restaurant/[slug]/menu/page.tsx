5913f1de5f7113d3b5a66e3b9fa5318f68e925f9
codecamjam <camerontaghlabi@gmail.com>
2023-04-07 00:10:54 -0400

    l19-21: dry code, layouts, and nesting

--- app/restaurant/[slug]/menu/page.tsx
+++ app/restaurant/[slug]/menu/page.tsx
@@ -6,17 +6,9 @@ import Menu from "../components/Menu";
 
 export default function RestaurantMenu() {
   return (
-    <main className="bg-gray-100 min-h-screen w-screen">
-      <main className="max-w-screen-2xl m-auto bg-white">
-        <NavBar />
-        <Header />
-        <div className="flex m-auto w-2/3 justify-between items-start 0 -mt-11">
-          <div className="bg-white w-[100%] rounded p-3 shadow">
-            <RestaurantNavBar />
-            <Menu />
-          </div>
-        </div>
-      </main>
-    </main>
+    <div className="bg-white w-[100%] rounded p-3 shadow">
+      <RestaurantNavBar />
+      <Menu />
+    </div>
   );
 }
